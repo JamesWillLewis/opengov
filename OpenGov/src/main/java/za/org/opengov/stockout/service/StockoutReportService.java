cde87e672614a03e2437f5c397ae2f0f7dac0b4e
james <james.will.lewis@gmail.com>
2013-09-07 12:45:03 +0200

    Added submit stockout-report business function to service

--- OpenGov/src/main/java/za/org/opengov/stockout/service/StockoutReportService.java
+++ OpenGov/src/main/java/za/org/opengov/stockout/service/StockoutReportService.java
@@ -1,5 +1,15 @@
 package za.org.opengov.stockout.service;
 
+import za.org.opengov.stockout.entity.StockoutReport;
+
 public interface StockoutReportService {
+	
+	/**
+	 * Submit a stock-out report.
+	 * 
+	 * @param stockoutReport
+	 * @return Handle for the report (primary key)
+	 */
+	public long submitStockoutReport(StockoutReport stockoutReport);
 
 }
--- OpenGov/src/main/java/za/org/opengov/stockout/service/impl/StockoutReportServiceImpl.java
+++ OpenGov/src/main/java/za/org/opengov/stockout/service/impl/StockoutReportServiceImpl.java
@@ -3,10 +3,19 @@ package za.org.opengov.stockout.service.impl;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import za.org.opengov.stockout.entity.StockoutReport;
 import za.org.opengov.stockout.service.StockoutReportService;
 
 @Service("stockoutReportService")
 @Transactional
 public class StockoutReportServiceImpl implements StockoutReportService {
+
+	@Override
+	public long submitStockoutReport(StockoutReport stockoutReport) {
+
+		return 0;
+	}
+	
+	
 
 }
