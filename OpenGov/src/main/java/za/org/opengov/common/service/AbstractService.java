d23be729a590ecf87aae731587c23d8e7e5754da
james <james.will.lewis@gmail.com>
2013-10-03 00:06:12 +0200

    Added service interface, which includes CRUD operations.

---
+++ OpenGov/src/main/java/za/org/opengov/common/service/AbstractService.java
@@ -0,0 +1,16 @@
+package za.org.opengov.common.service;
+
+import java.io.Serializable;
+import java.util.List;
+
+public interface AbstractService<E, I extends Serializable> {
+
+	public E get(I identifier);
+	
+	public void remove(E entity);
+	
+	public List<E> getAll();
+	
+	public void put(E entity);
+	
+}
